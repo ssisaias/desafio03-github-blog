6d295042f3c403a04f6719029e99c83e17087fcd
ssisaias <ssisaias@users.noreply.github.com>
2024-03-17 00:10:16 -0300

    Removes dtmoney modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,25 +1,13 @@
-import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
-import * as Dialog from "@radix-ui/react-dialog";
+import { HeaderContainer, HeaderContent } from "./styles";
 
 import logoImg from "../../assets/logo.svg";
-import { NewTransactionModal } from "../NewTransactionModal";
-import { useState } from "react";
 
 export function Header() {
-  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
 
   return (
     <HeaderContainer>
       <HeaderContent>
         <img src={logoImg} alt="" />
-
-        <Dialog.Root open={isNewTransactionModalOpen} onOpenChange={setIsNewTransactionModalOpen}>
-          <Dialog.Trigger asChild>
-            <NewTransactionButton>Nova transação</NewTransactionButton>
-          </Dialog.Trigger>
-          <NewTransactionModal onRequestClose={() => setIsNewTransactionModalOpen(false)} />
-        </Dialog.Root>
-
       </HeaderContent>
     </HeaderContainer>
   );
